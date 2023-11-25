3804691463e89fe38f389b5355cad3918c892428
Mariam4324 <ashkmariam@gmail.com>
2023-11-25 23:02:50 +0300

    16 commit

--- js/script.js
+++ js/script.js
@@ -1,110 +1,19 @@
-// var name = "Mariam";
-// let number = 1;
-// const pi = 3.14;
+const arr = [
+  {
+    name: "mariam",
+    age: 18,
+  },
+  {
+    name: "mariam",
+    age: 18,
+  },
+  {
+    name: "mariam",
+    age: 18,
+  },
+];
 
-// let LeftBorderWidth = 200;
-
-// ТИПЫ ДАННЫХ
-// 1. number
-// 2. string - строка, любая инфа словами "" '' ``
-// 3. true/false
-// 4. null - чего-то просто не существует в природе
-// 5. undefined - чтото существует но не имеет никакого значения
-// 6. obj = {
-//     // обьект
-//     name: 'apple'
-//     color: 'green'
-//     weight: 200
-// }
-// 7. symbol технический, редко
-
-// методы
-// 1. alert(123) - выводит инфу на страницу, редко
-// 2. console.log("cccscc") инфа появится в консоли, нужен только для разработчика
-// 3. console.log(number); - выводится переменная в консоли в дел тулс
-// 4. confirm("вам есть 18?")  - выведется окно как в алерт с вопросом
-
-// let answer = confirm("вам есть 18?");
-// console.log(answer);
-
-// let answer = prompt("вам есть 18?", "");
-// console.log(answer);
-
-// ОПЕРАТОРЫ
-// console.log(4 + 3);
-// И ИЛИ
-// let IsChecked = true,
-//     IsClosed = true;
-// И
-// console.log(IsChecked && IsClosed);
-// ИЛИ
-//  console.log(IsChecked || IsClosed);
-
-// if (2 * 4 == 8 * 1) {
-//     console.log('верно')
-// } else {
-//     console.log("neverno")
-// }
-
-// for (let i = 1; i < 9; i++) {
-//     console.log(i);
-// }
-
-// prompt("Введите 2 числа");
-
-// let i = prompt("")
-// console.log(i);
-// 1
-// let a = prompt("Введите первое число");
-// let b = prompt("Второе число");
-
-// if (a < b) {
-//   for (; a < b; a++) {
-//     console.log(a);
-//   }
-// } else console.log("ошибка");
-// 2
-// let a = +prompt("с какого числа начать?");
-// let b = +prompt("до какого числа?");
-// let c = +prompt("на сколько увеличивать каждый раз?");
-
-// if (a > b) {
-//   console.log("ошибка");
-// } else {
-//   for (; a < b; a + c) {
-//     console.log(a);
-//   }
-// }
-// 3
-// let a = prompt("Введите любое положительное число")
-
-// if (a < 0) {
-//     console.log("ошибка");
-// } else {
-//     for (; a >= 0; a--) {
-//         console.log(a);
-//     }
-// }
-// 4
-// let a = prompt("Любое положительное число")
-
-// if (a < 0) {
-//     console.log("ошибка");
-// } else {
-//     while () {
-
-//     }
-// }
-// 5
-
-// let a = prompt("угадай число");
-
-// if (a < 0) {
-//   console.log("ошибка");
-// } else {
-//   while (a != 100) {
-//     a = +prompt("угадай число");
-//     console.log(a);
-//   } 
-// }
-
+arr.forEach((el) => {
+  const { name, age } = el;
+  console.log(name, age);
+});
